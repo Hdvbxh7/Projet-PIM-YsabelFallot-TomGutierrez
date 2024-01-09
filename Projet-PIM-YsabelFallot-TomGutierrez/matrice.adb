f475cc9245ecc08d3c2bb3c2849c30ed31fb9cf8
FALLOT <ysabel.fallot@etu-inp.n7.fr>
2024-01-09 11:01:50 +0100

    chgt matrice.adb

--- Projet-PIM-YsabelFallot-TomGutierrez/matrice.adb
+++ Projet-PIM-YsabelFallot-TomGutierrez/matrice.adb
@@ -1,8 +1,5 @@
 with Ada.Text_IO;            use Ada.Text_IO;
-with Ada.Integer_Text_IO;       use Ada.Integer_Text_IO;
-with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
 with Matrice_Exceptions;         use Matrice_Exceptions;
-with Matrice;
 
 package body Matrice is
 	
